cd957b0bf3f52d1f0662c79a347efd1ece7758ae
Heiko Seeberger <mail@heikoseeberger.de>
2015-03-05 09:31:09 +0100

    Scale times for Travis CI

--- akka-sse/src/test/scala/de/heikoseeberger/akkasse/HeartbeatSourceSpec.scala
+++ akka-sse/src/test/scala/de/heikoseeberger/akkasse/HeartbeatSourceSpec.scala
@@ -16,25 +16,21 @@
 
 package de.heikoseeberger.akkasse
 
-import akka.actor.{ ActorSystem, Props }
-import akka.stream.ActorFlowMaterializer
+import akka.actor.Props
 import akka.stream.actor.ActorPublisher
 import akka.stream.scaladsl.{ FlowGraph, Merge, Sink, Source }
-import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpec }
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{ Millis, Span }
-import scala.concurrent.duration._
+import scala.concurrent.duration.DurationInt
 
-class HeartbeatSourceSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
+class HeartbeatSourceSpec extends BaseSpec with ScalaFutures {
 
-  implicit val system = ActorSystem()
-  implicit val flowMaterializer = ActorFlowMaterializer()
+  import EventStreamMarshalling._
 
   "heart beats" should {
 
     "be merged into event stream" in {
-      import de.heikoseeberger.akkasse.EventStreamMarshalling._
       implicit def intToServerSentEvent(n: Int): ServerSentEvent = ServerSentEvent(n.toString)
 
       val eventPublisher = system.actorOf(Props(new EventPublisher[Int](10) {
@@ -47,7 +43,6 @@ class HeartbeatSourceSpec extends WordSpec with Matchers with BeforeAndAfterAll 
 
       val source = Source() { implicit b =>
         import FlowGraph.Implicits._
-
         val merge = b.add(Merge[ServerSentEvent](inputPorts = 2))
         merge <~ events
         merge <~ heartbeats
@@ -56,19 +51,11 @@ class HeartbeatSourceSpec extends WordSpec with Matchers with BeforeAndAfterAll 
 
       for (n <- 1 to 4) eventPublisher ! 42
 
-      whenReady(source.grouped(8).runWith(Sink.head), Timeout(Span(300, Millis))) { result =>
+      whenReady(source.grouped(8).runWith(Sink.head), Timeout(scaled(Span(300, Millis)))) { result =>
         val events = result.groupBy(_.data)
         events("42").size shouldBe 4
         events("heartbeat").size shouldBe 4
       }
     }
-
   }
-
-  override protected def afterAll() = {
-    super.afterAll()
-    system.shutdown()
-    system.awaitTermination()
-  }
-
 }
